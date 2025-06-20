92e21084188edb0b0af0e94e826daa15094a1255
Cobithree <168717343+Cobithree@users.noreply.github.com>
2025-06-20 22:06:07 +0200

    Add files via upload

--- netlify/functions/submitResult.js
+++ netlify/functions/submitResult.js
@@ -6,7 +6,14 @@ exports.handler = async function (event) {
   }
 
   const { nick, resultType, portfolioValue, returnPercent } = JSON.parse(event.body);
-
+  console.log("⬇️ Dane otrzymane z frontendu:", {
+  nick,
+  resultType,
+  portfolioValue,
+  returnPercent
+});
+	
+	
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
@@ -19,10 +26,15 @@ exports.handler = async function (event) {
       VALUES ($1, $2, $3, $4)
     `;
     await client.query(query, [nick, resultType, portfolioValue, returnPercent]);
+	console.log("✅ Dane zapisane pomyślnie do Neon");
     await client.end();
 
     return { statusCode: 200, body: JSON.stringify({ message: 'Zapisano wynik' }) };
   } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
-  }
+		console.error("❌ Błąd zapisu do Neon:", error);
+		return {
+			statusCode: 500,
+			body: JSON.stringify({ error: error.message })
+  };
+}
 };
