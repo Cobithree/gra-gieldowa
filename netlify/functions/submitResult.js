640c1ece96725b7584f07021bb3e8ad5803c6479
Cobithree <168717343+Cobithree@users.noreply.github.com>
2025-06-20 21:26:13 +0200

    Add files via upload

---
+++ netlify/functions/submitResult.js
@@ -0,0 +1,28 @@
+const { Client } = require('pg');
+
+exports.handler = async function (event) {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { nick, resultType, portfolioValue, returnPercent } = JSON.parse(event.body);
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
+
+  try {
+    await client.connect();
+    const query = `
+      INSERT INTO results (nick, result_type, portfolio_value, return_percent)
+      VALUES ($1, $2, $3, $4)
+    `;
+    await client.query(query, [nick, resultType, portfolioValue, returnPercent]);
+    await client.end();
+
+    return { statusCode: 200, body: JSON.stringify({ message: 'Zapisano wynik' }) };
+  } catch (error) {
+    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
+  }
+};
